2d038b711bf6d237e22314de03e56ee55859d722
Evan Dempsey <evandempsey@gmail.com>
2014-06-28 16:34:24 +0100

    Observer update.

--- patterns/observer.py
+++ patterns/observer.py
@@ -1,22 +1,6 @@
-#!/usr/bin/env python
-
-
-# Create an abstract class for observers.
-
-
-class Observer(object):
-    """
-    Abstract class to respond to changes in the subject.
-    """
-
-    def update(self):
-        """
-        Update observer state.
-        """
-        raise NotImplementedError("update() is not implemented.")
-
-
-# Create a concrete class for the subject.
+# A demonstration of how the Observer Pattern might
+# be coded in a more Pythonic way, with __setattr__
+# on the subject overridden to update the observers.
 
 
 class Subject(object):
@@ -24,35 +8,37 @@ class Subject(object):
     Maintains a list of observers and notifies
     them of state changes.
     """
-
     def __init__(self):
         """
         Initialize a list of observers.
         """
-        self.state = 0
-        self.observers = []
+        self.__dict__['state'] = 0
+        self.__dict__['observers'] = []
 
-    def getState(self):
+    def __setattr__(self, name, value):
         """
-        Return the state of the subject.
+        Override to notify observers of state
+        changes when the state is updated.
         """
-        return self.state
+        self.__dict__[name] = value
+        if name == 'state':
+            self.notify_observers()
 
-    def setState(self, state):
-        """
-        Set the state of the subject and notify observers.
-        """
-        self.state = state
-        self.notifyObservers()
-
-    def attach(self, observer):
+    def register(self, observer):
         """
         Add an observer to the list of observers
         to be notified of state changes.
         """
-        self.observers.append(observer)
+        if observer not in self.observers:
+            self.observers.append(observer)
 
-    def notifyObservers(self):
+    def deregister(self, observer):
+        """
+        Remove an observer.
+        """
+        self.observers.remove(observer)
+
+    def notify_observers(self):
         """
         Iterate through observers and call the
         update() method on each one.
@@ -61,55 +47,62 @@ class Subject(object):
             observer.update()
 
 
-# Create concrete classes for observers.
+# Create an abstract base class for and concrete classes for observers.
+
+class Observer(object):
+    """
+    Abstract class to respond to changes in the subject.
+    """
+    def update(self):
+        """
+        Update observer state.
+        """
+        raise NotImplementedError("update() is not implemented.")
 
 
 class BinaryObserver(Observer):
     """
     Observer that prints subject state in binary.
     """
-
     def __init__(self, subject):
         """
         Keep a reference to the subject.
         """
         self.subject = subject
-        self.subject.attach(self)
+        self.subject.register(self)
 
     def update(self):
-        print bin(self.subject.getState())
+        print bin(self.subject.state)
 
 
 class OctalObserver(Observer):
     """
     Observer that prints subject state in octal.
     """
-
     def __init__(self, subject):
         """
         Keep a reference to the subject.
         """
         self.subject = subject
-        self.subject.attach(self)
+        self.subject.register(self)
 
     def update(self):
-        print oct(self.subject.getState())
+        print oct(self.subject.state)
 
 
 class HexadecimalObserver(Observer):
     """
     Observer that prints subject state in hexadecimal.
     """
-
     def __init__(self, subject):
         """
         Keep a reference to the subject.
         """
         self.subject = subject
-        self.subject.attach(self)
+        self.subject.register(self)
 
     def update(self):
-        print hex(self.subject.getState())
+        print hex(self.subject.state)
 
 
 def main():
@@ -122,10 +115,10 @@ def main():
     HexadecimalObserver(subject)
 
     print "First state change."
-    subject.setState(10)
+    subject.state = 10
 
     print "Second state change."
-    subject.setState(168438)
+    subject.state = 168438
 
 
 if __name__ == "__main__":
